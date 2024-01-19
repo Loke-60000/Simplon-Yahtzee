72279800ce672120bf53c8699d68f7a67e1dbe44
jimmy fernandez <jimmy.fernandez.devia@gmail.com>
2024-01-19 10:50:36 +0100

    ajoute fichier test_function_roll_dice

--- test_main.py
+++ test_main.py
@@ -1,17 +1,6 @@
-import unittest
-from unittest.mock import patch
-from main import dices
+from main import *
 
-class TestDices(unittest.TestCase):
+def test_game():
+    pass
 
-    @patch('builtins.input', side_effect=['2', ' ', ' ', ' ', '1', ' ', ' '])  
-    @patch('random.randint', return_value=3) 
-    def test_dices_without_human_input(self, mock_randint, mock_input):
-        try:
-            dices()
-            print('Test OK') 
-        except Exception as e:
-            self.fail(f"Test failed due to an unexpected error: {e}")
-
-if __name__ == '__main__':
-    unittest.main()
+assert test_game()
